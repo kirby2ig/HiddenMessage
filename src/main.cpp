e37b5517080368171ca3deb2245991eb2d99e120
Ben Marshall <kirby2ig88@gmail.com>
2019-04-01 16:56:50 -0400

    Finish implementing 2D arrays

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,6 @@ int main(int argc, char* argv[])
 		strcpy(*(randomText + i - 1), argv[i]);
 	}
 	
-	std::cout << length << std::endl;
-
 	//Create a buffer for the text we are writing in the center
 	char** centerDisplay = new char*[argc - 1];
 	for(int i = 0; i < argc - 1; i++)
@@ -40,22 +38,23 @@ int main(int argc, char* argv[])
 	}
 
 	//Randomize the order that we reveal the characters
-	int** randomIndexOrder = new int*[length];
-	for(int i = 0; i < length; i ++)
+	int** randomIndexOrder = new int*[argc - 1];
+	for(int i = 0; i < argc - 1; i ++)
 	{
-		int len = strlen(*(randomText + i)); // Need to convert remaining code to use 2d arrays for multiple lines
-	}
-	for(int i = 0; i < length; i++)
-	{
-		*(randomIndexOrder + i) = i;
-	}
-	//Shuffle
-	for(int i = length - 1; i > 0; i--)
-	{
-		int j = rand() % i;
-		int temp = *(randomIndexOrder + i);
-		*(randomIndexOrder + i) = *(randomIndexOrder + j);
-		*(randomIndexOrder + j) = temp;
+		int len = strlen(*(randomText + i));
+		*(randomIndexOrder + i) = new int[len];
+		for(int j = 0; j < len; j++)
+		{
+			*(*(randomIndexOrder + i) + j) = j;
+		}
+		//Shuffle
+		for(int j = len - 1; j > 0; j--)
+		{
+			int k = rand() % j;
+			int temp = *(*(randomIndexOrder + i) + j);
+			*(*(randomIndexOrder + i) + j) = *(*(randomIndexOrder + i) + k);
+			*(*(randomIndexOrder + i) + k) = temp;
+		}
 	}
 
 	//Initialize screen
@@ -66,37 +65,68 @@ int main(int argc, char* argv[])
 
 	//Main loop
 	bool run = true;
-	int lettersShown = 0;
+	int* lettersShown = new int[argc - 1];
+	for(int i = 0; i < argc - 1; i++)
+	{
+		*(lettersShown + i) = 0;
+	}
 	while(run)
 	{
 		//Handle input
 		char ch = window.getTypedCharacter();
 		if(ch == 'q')
 			run = false;
+
 		//Randomize message
-		if(rand() < (RAND_MAX / (FPS / 3)) && lettersShown < length)
+		if(rand() < (RAND_MAX / (FPS / 3)))
 		{
-			lettersShown++;
+			for(int i = 0; i < argc - 1; i++)
+			{
+				if(*(lettersShown + i) < strlen(*(randomText + i)))
+					(*(lettersShown + i))++;
+			}
 		}
-		for(int i = 0; i < lettersShown; i++)
+		for(int i = 0; i < argc - 1; i++)
 		{
-			int j = *(randomIndexOrder + i);
-			*(centerDisplay + j) = *(randomText + j); 
+			for(int j = 0; j < *(lettersShown + i); j++)
+			{
+				int k = *(*(randomIndexOrder + i) + j);
+				*(*(centerDisplay + i) + k) = *(*(randomText + i) + k);
+			}
 		}
 		//Draw text
 		window.randomize();
-		window.printTextCenteredGreen(centerDisplay, length);
+		for(int i = 0; i < argc - 1; i++)
+		{
+			// void printText(char* text, int length, int x, int y, int color);
+			int len = strlen(*(centerDisplay + i));
+			int x = window.getWidth() / 2 - len / 2;
+			int y = window.getHeight() / 2 - argc - 1 + i;
+			window.printText(*(centerDisplay + i), len, x, y, GREEN_COLOR);
+		}
 		window.draw();
 		usleep(FRAMETIME);
 	}
 
+	for(int i = 0; i < argc - 1; i++)
+	{
+		delete[] *(centerDisplay + i);
+	}
 	delete[] centerDisplay;
+
+	for(int i = 0; i < argc - 1; i++)
+	{
+		delete[] *(randomIndexOrder + i);
+	}
 	delete[] randomIndexOrder;
+	
 	for(int i = 0; i < argc - 1; i++)
 	{
 		delete[] *(randomText + i);
 	}
 	delete[] randomText;
+
+	delete[] lettersShown;
 
 	return 0;
 }
